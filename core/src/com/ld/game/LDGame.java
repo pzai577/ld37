8815df9f86c6063b6dc5e7fe0809a87cea65df89
Anderson Wang <anderson.wang1994@gmail.com>
2016-12-09 23:23:11 -0500

    adding a tileset+test level

--- core/src/com/ld/game/LDGame.java
+++ core/src/com/ld/game/LDGame.java
@@ -3,31 +3,42 @@ package com.ld.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class LDGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private TiledMap tileMap;
+	private OrthogonalTiledMapRenderer tileMapRenderer;
+	private SpriteBatch batch;
+	private OrthographicCamera cam;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false, 480, 320);
+		batch.setProjectionMatrix(cam.combined);
+		tileMap = new TmxMapLoader().load("test_level.tmx");
+		tileMapRenderer = new OrthogonalTiledMapRenderer(tileMap);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
+		tileMapRenderer.setView(cam);
+		tileMapRenderer.render();
+		
+		cam.update();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
