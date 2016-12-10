9df16ff8ab538b7a091b97ff1e663d81ba57c09f
Nathan Pinsker <npinsker@gmail.com>
2016-12-09 20:01:50 -0800

    movement

--- core/src/com/ld/game/LDGame.java
+++ core/src/com/ld/game/LDGame.java
@@ -2,32 +2,50 @@ package com.ld.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class LDGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	
+	Rectangle player = new Rectangle(200, 200, 112, 112);
+	Texture playerImg;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		playerImg = new Texture("mayuri.jpg");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			player.x -= 3;
+		}
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			player.x += 3;
+		}
+		if (Gdx.input.isKeyPressed(Keys.UP)) {
+			player.y += 3;
+		}
+		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+			player.y -= 3;
+		}
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(playerImg, player.x, player.y);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		playerImg.dispose();
 	}
 }
