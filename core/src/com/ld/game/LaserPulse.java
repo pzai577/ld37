addecf1c155449c24756923da53362e9958085d8
Phillip Ai <pzai.577@gmail.com>
2016-12-11 13:15:19 -0500

    made lasers more laser-like

--- core/src/com/ld/game/LaserPulse.java
+++ core/src/com/ld/game/LaserPulse.java
@@ -1,34 +1,58 @@
 package com.ld.game;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.ld.game.Player;
 
 public class LaserPulse extends Projectile {
 	
 	Player player;
+	Rectangle pulsebox;
+	
+	float laserLength = 50;
+	float laserThickness = 2;
+	float speed = 5;
+	float pulseSize = 2;
+	
+	Color laserColor = Color.CYAN;
+	Color pulseColor = new Color(0f, 0.5f, 0.5f, 1f);
 	
 	public LaserPulse(Player player, boolean shootingRight){
 		super();
 		
 		this.player = player;
 		
-		float laserLength = 50;
-		float laserThickness = 2;
-		float speed = 5;
-		Rectangle hitbox;
 		if(shootingRight){
-			hitbox = new Rectangle(player.position.x + Player.PLAYER_WIDTH, player.position.y + 20, laserLength, laserThickness);
+			this.hitbox = new Rectangle(player.position.x + Player.PLAYER_WIDTH, player.position.y + 20, laserLength, laserThickness);
 			this.horizVelocity = speed;
 		}
 		else{
-			hitbox = new Rectangle(player.position.x - laserLength, player.position.y + 20, laserLength, laserThickness);
+			this.hitbox = new Rectangle(player.position.x - laserLength, player.position.y + 20, laserLength, laserThickness);
 			this.horizVelocity = -speed;
 		}
 		
-		this.color = Color.CYAN;
-		this.hitboxes.add(hitbox);
+		this.pulsebox = new Rectangle(hitbox.x - pulseSize, hitbox.y - pulseSize, laserLength + 2 * pulseSize, laserThickness + 2 * pulseSize);
 		
+//		this.colors.add();
+//		this.hitboxes.add(pulsebox);
+//		this.colors.add(Color.CYAN);
+//		this.hitboxes.add(hitbox);
+	}
+	
+	@Override
+	public void update() {
+		super.update();
+		updateBox(pulsebox);
+	}
+	
+	@Override
+	public void render(ShapeRenderer r) {
+		// TODO: make pulsebox rounded and pulse
+		r.setColor(pulseColor);
+		renderRectHelper(r, pulsebox);
+		r.setColor(laserColor);
+		renderRectHelper(r, hitbox);
 	}
 
 }
--- core/src/com/ld/game/Projectile.java
+++ core/src/com/ld/game/Projectile.java
@@ -1,14 +1,16 @@
 package com.ld.game;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 
 public class Projectile {
 	
-	protected Array<Rectangle> hitboxes;
-	
-	public Color color;
+//	protected Array<Rectangle> hitboxes;
+//	
+//	public Array<Color> colors;
+	public Rectangle hitbox;
 	
 	protected double horizVelocity;
 	protected double vertVelocity;
@@ -16,8 +18,9 @@ public class Projectile {
 	protected double vertAccel;
 	
 	public Projectile() {
-		this.hitboxes = new Array<Rectangle>();
-		this.color = null;
+//		this.hitboxes = new Array<Rectangle>();
+//		this.colors = new Array<Color>();
+		this.hitbox = new Rectangle();
 		this.horizVelocity = 0;
 		this.vertVelocity = 0;
 		this.horizAccel = 0;
@@ -25,12 +28,26 @@ public class Projectile {
 	}
 	
 	public void update() {
-		for(Rectangle box: hitboxes) {
-			box.x += this.horizVelocity;
-			box.y -= this.vertVelocity;
-		}
+//		for(Rectangle box: hitboxes) {
+//			box.x += this.horizVelocity;
+//			box.y -= this.vertVelocity;
+//		}
+		updateBox(hitbox);
 		horizVelocity += horizAccel;
 		vertVelocity += vertAccel;
+	}
+	
+	protected void updateBox(Rectangle rect) {
+		rect.x += this.horizVelocity;
+		rect.y -= this.vertVelocity;
+	}
+	
+	public void render(ShapeRenderer r){
+		return;
+	}
+	
+	protected void renderRectHelper(ShapeRenderer r, Rectangle rect) {
+		r.rect(rect.x, rect.y, rect.width, rect.height);
 	}
 
 }
