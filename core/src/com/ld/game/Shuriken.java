6f689ee420f4cf691f061109ebf49cb99824ff16
Phillip Ai <pzai.577@gmail.com>
2016-12-22 23:08:06 -0500

    added the file ._.

---
+++ core/src/com/ld/game/Shuriken.java
@@ -0,0 +1,67 @@
+package com.ld.game;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Shuriken extends Projectile {
+    
+    Player player;
+    
+    float speed = 6;
+    
+    Color centerColor = Color.BLACK;
+    
+    public Shuriken(Map map, Player player, boolean shootingRight){
+        super(map);
+        this.player = player;
+        
+        if(shootingRight){
+            this.hitbox = new HurtboxRectangle(new Rectangle(player.position.x + Player.PLAYER_WIDTH, player.position.y + 20, 20, 20), this);
+            this.head = new Vector2(hitbox.x+10, hitbox.y+10);
+            this.horizVelocity = speed;
+        }
+        else{
+            this.hitbox = new HurtboxRectangle(new Rectangle(player.position.x - 21, player.position.y + 20, 20, 20), this);
+            this.head = new Vector2(hitbox.x+10, hitbox.y+10);
+            this.horizVelocity = -speed;
+        }
+    }
+    
+    @Override
+    public void update() {
+        super.update();
+        if(this.intersectPlayer()){
+            this.destroy();
+            player.grabShuriken();
+        }
+    }
+    
+    private boolean intersectPlayer() {
+        return Intersector.overlaps(player.position, this.hitbox);
+    }
+    
+    @Override
+    public void render(ShapeRenderer r) {
+        r.setColor(centerColor);
+        renderRectHelper(r, this.hitbox);
+    }
+    
+    public void stop() {
+        this.horizVelocity = 0;
+        this.vertVelocity = 0;
+    }
+    
+//    @Override
+//    public void destroy() {
+//        super.destroy();
+//    }
+    
+    @Override
+    public void handleWallCollision() {
+        this.stop();
+    }
+
+}
